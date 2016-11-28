176e518e1e66233221a2fed0745b6085c4c677d8
Breno Oliveira dos Santos <breno.santos@tmax.com.br>
2016-11-28 18:02:21 -0200

    projeto - fase 1

--- app.js
+++ app.js
@@ -1,3 +1,22 @@
+Vue.filter('doneLabel', function(value){
+    if(value == false){
+        return "Não paga";
+    }else{
+        return "Paga";
+    }
+});
+
+Vue.filter('corContas', function(value){
+    if(value === false){
+        return "Nenhuma conta Cadastrada";
+    }
+    if(!value){
+        return "Nenhuma conta a pagar"
+    }else{
+        return "Você tem "+value+" contas a pagar";
+    }
+});
+
 var app = new Vue({
     el: "#app",
     data:{
@@ -7,13 +26,13 @@ var app = new Vue({
             {id: 0, name:"Listar Contas"},
             {id: 1, name: "Criar Conta"}
         ],
-        activedView: 1,
+        activedView: 0,
         formType: 'insert',
         bill: {
             data_due: '',
             name: '',
             value: 0,
-            done: 0
+            done: false
         },
         names: [
             'Conta de luz',
@@ -25,24 +44,27 @@ var app = new Vue({
             'Gasolina',
         ],
         bills: [
-            {data_due: '20/08/2016', name: 'Conta de luz', value: 70.99, done: 1},
-            {data_due: '21/08/2016', name: 'Conta de água', value: 55.99, done:0},
-            {data_due: '22/08/2016', name: 'Conta de telefone', value: 25.99, done:0},
-            {data_due: '23/08/2016', name: 'Supermercado', value: 65.99, done:0},
-            {data_due: '24/08/2016', name: 'Cartão de crédito', value: 1500.99, done:0},
-            {data_due: '25/08/2016', name: 'Empréstimo', value: 5500.99, done:0},
-            {data_due: '26/08/2016', name: 'Gasolina', value: 200.99, done:0}
+            {data_due: '20/08/2016', name: 'Conta de luz', value: 70.99, done: true},
+            {data_due: '21/08/2016', name: 'Conta de água', value: 55.99, done: false},
+            {data_due: '22/08/2016', name: 'Conta de telefone', value: 25.99, done: false},
+            {data_due: '23/08/2016', name: 'Supermercado', value: 65.99, done: false},
+            {data_due: '24/08/2016', name: 'Cartão de crédito', value: 1500.99, done: false},
+            {data_due: '25/08/2016', name: 'Empréstimo', value: 5500.99, done: false},
+            {data_due: '26/08/2016', name: 'Gasolina', value: 200.99, done: false}
         ]
     },
     computed: {
         status: function(){
+            if(this.bills == ""){
+                return false;
+            }
             var count = 0;
             for(var i in this.bills){
                 if(!this.bills[i].done){
                     count++;
                 }
             }
-            return !count?"Nenhuma conta a Pagar":"Existem "+ count +" a serem pagas";
+            return count;
         }
     },
     methods: {
@@ -60,7 +82,7 @@ var app = new Vue({
                 data_due: '',
                 name: '',
                 value: 0,
-                done: 0
+                done: false
             };
             this.activedView = 0;
         },
@@ -68,14 +90,11 @@ var app = new Vue({
             this.bill = bill;
             this.activedView = 1;
             this.formType = 'update'
-        }
-    }
+        },
+        remover: function(index){
+            if(confirm("deseja exlucir?")){
+                this.bills.splice(index, 1);
+            }
+        },
+    },
 });
-
-Vue.filter('doneLabel', function(value){
-    if(value == 0){
-        return "Não paga";
-    }else{
-        return "Paga";
-    }
-})
