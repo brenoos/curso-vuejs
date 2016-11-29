d843f54c58ff1fc01954fe0eede7b4fd48bd9cdc
Breno Oliveira dos Santos <breno.santos@tmax.com.br>
2016-11-29 12:06:05 -0200

    BillCreateComponent

--- app.js
+++ app.js
@@ -44,6 +44,120 @@ var menuComponent = Vue.extend({
     },
 });
 Vue.component('menu-component', menuComponent);
+var billListComponent = Vue.extend({
+    template: `
+        <style type="text/css">
+        .pago{
+            color: green;
+        }
+        .nao-pago{
+            color: red;
+        }
+        </style>
+        <table border="1" cellpadding="10">
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Vencimento</th>
+                    <th>Nome</th>
+                    <th>Valor</th>
+                    <th>Paga?</th>
+                    <th>Ações</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="(index,o) in bills">
+                    <td> {{index + 1}} </td>
+                    <td> {{o.data_due}} </td>
+                    <td> {{o.name}} </td>
+                    <td> {{o.value | currency 'R$ ' 2}} </td>
+                    <td class="minha-classe" :class="{'pago': o.done, 'nao-pago': !o.done}"> 
+                        {{o.done | doneLabel}} 
+                    </td>
+                    <td>
+                        <a href="#" @click.prevent="loadBill(o)">Editar</a> <a href="#" @click.prevent='remover(index)'>Remover</a>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    `,
+    data: function(){
+        return {
+            bills: [
+                {data_due: '20/08/2016', name: 'Conta de luz', value: 70.99, done: true},
+                {data_due: '21/08/2016', name: 'Conta de água', value: 55.99, done: false},
+                {data_due: '22/08/2016', name: 'Conta de telefone', value: 25.99, done: false},
+                {data_due: '23/08/2016', name: 'Supermercado', value: 65.99, done: false},
+                {data_due: '24/08/2016', name: 'Cartão de crédito', value: 1500.99, done: false},
+                {data_due: '25/08/2016', name: 'Empréstimo', value: 5500.99, done: false},
+                {data_due: '26/08/2016', name: 'Gasolina', value: 200.99, done: false}
+            ]
+        };
+    },
+    methods: {
+        loadBill: function(bill){
+            this.$parent.bill = bill;
+            this.$parent.activedView = 1;
+            this.$parent.formType = 'update'
+        },
+        remover: function(index){
+            if(confirm("deseja exlucir?")){
+                this.bills.splice(index, 1);
+            }
+        },
+    }
+});
+Vue.component('bill-list-component', billListComponent);
+var billCreateComponent = Vue.extend({
+    template:`
+        <form name="form" @submit.prevent="submit">
+            <label>Vencimento:</label>
+            <input type="text" v-model="bill.data_due" />
+            <br /><br />
+            <label>Nome:</label>
+            <select v-model="bill.name">
+                <option v-for="o in names" :value="o"> {{o}} </option>
+            </select>
+            <br /><br />
+            <label>Valor:</label>
+            <input type="text" v-model="bill.value"/>
+            <br /><br />
+            <label> Paga</label>
+            <input type="checkbox" v-model="bill.done" />
+            <br /><br />
+            <input type="submit" value="Enviar"/>
+        </form>    
+    `,
+    props: ['bill','formType'],
+    data: function(){
+        return {
+            names: [
+                'Conta de luz',
+                'Conta de água',
+                'Conta de telefone',
+                'Supermercado',
+                'Cartão de crédito',
+                'Empréstimo',
+                'Gasolina',
+            ]
+        };
+    },
+    methods: {
+        submit: function(){
+            if(this.formType == 'insert'){
+                this.bills.push(this.bill);
+            }
+            this.bill = {
+                data_due: '',
+                name: '',
+                value: 0,
+                done: false
+            };
+            this.activedView = 0;
+        },
+    }
+});
+Vue.component('bill-create-component',billCreateComponent);
 var appComponent = Vue.extend({
     template: `
     <style type="text/css">
@@ -66,51 +180,10 @@ var appComponent = Vue.extend({
     </h3>
     <menu-component></menu-component>
     <div v-if="activedView == 0">
-    <table border="1" cellpadding="10">
-        <thead>
-            <tr>
-                <th>#</th>
-                <th>Vencimento</th>
-                <th>Nome</th>
-                <th>Valor</th>
-                <th>Paga?</th>
-                <th>Ações</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr v-for="(index,o) in bills">
-                <td> {{index + 1}} </td>
-                <td> {{o.data_due}} </td>
-                <td> {{o.name}} </td>
-                <td> {{o.value | currency 'R$ ' 2}} </td>
-                <td class="minha-classe" :class="{'pago': o.done, 'nao-pago': !o.done}"> 
-                    {{o.done | doneLabel}} 
-                </td>
-                <td>
-                    <a href="#" @click.prevent="loadBill(o)">Editar</a> <a href="#" @click.prevent='remover(index)'>Remover</a>
-                </td>
-            </tr>
-        </tbody>
-    </table>
+        <bill-list-component></bill-list-component>
     </div>
     <div v-if="activedView == 1">
-        <form name="form" @submit.prevent="submit">
-            <label>Vencimento:</label>
-            <input type="text" v-model="bill.data_due" />
-            <br /><br />
-            <label>Nome:</label>
-            <select v-model="bill.name">
-                <option v-for="o in names" :value="o"> {{o}} </option>
-            </select>
-            <br /><br />
-            <label>Valor:</label>
-            <input type="text" v-model="bill.value"/>
-            <br /><br />
-            <label> Paga</label>
-            <input type="checkbox" v-model="bill.done" />
-            <br /><br />
-            <input type="submit" value="Enviar"/>
-        </form>
+       <bill-create-component :bill="bill" :form-type="formType"></bill-create-component>      
     </div>
     `,
     data: function(){
@@ -125,24 +198,6 @@ var appComponent = Vue.extend({
                 value: 0,
                 done: false
             },
-            names: [
-                'Conta de luz',
-                'Conta de água',
-                'Conta de telefone',
-                'Supermercado',
-                'Cartão de crédito',
-                'Empréstimo',
-                'Gasolina',
-            ],
-            bills: [
-                {data_due: '20/08/2016', name: 'Conta de luz', value: 70.99, done: true},
-                {data_due: '21/08/2016', name: 'Conta de água', value: 55.99, done: false},
-                {data_due: '22/08/2016', name: 'Conta de telefone', value: 25.99, done: false},
-                {data_due: '23/08/2016', name: 'Supermercado', value: 65.99, done: false},
-                {data_due: '24/08/2016', name: 'Cartão de crédito', value: 1500.99, done: false},
-                {data_due: '25/08/2016', name: 'Empréstimo', value: 5500.99, done: false},
-                {data_due: '26/08/2016', name: 'Gasolina', value: 200.99, done: false}
-            ]
         };
     },
     computed: {
@@ -160,28 +215,7 @@ var appComponent = Vue.extend({
         }
     },
     methods: {
-        submit: function(){
-            if(this.formType == 'insert'){
-                this.bills.push(this.bill);
-            }
-            this.bill = {
-                data_due: '',
-                name: '',
-                value: 0,
-                done: false
-            };
-            this.activedView = 0;
-        },
-        loadBill: function(bill){
-            this.bill = bill;
-            this.activedView = 1;
-            this.formType = 'update'
-        },
-        remover: function(index){
-            if(confirm("deseja exlucir?")){
-                this.bills.splice(index, 1);
-            }
-        },
+
     },
 });
 Vue.component('app-component', appComponent);
