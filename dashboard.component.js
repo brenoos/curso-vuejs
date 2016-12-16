784cf72c342531a839aa8957b293763b9760a0d0
Breno Oliveira dos Santos <breno.santos@tmax.com.br>
2016-12-16 15:06:13 -0200

    Projeto SPA(correcao)

--- dashboard.component.js
+++ dashboard.component.js
@@ -6,7 +6,7 @@ window.dashboardComponent = Vue.extend({
     <h2>Você tem {{totalPagar | currency 'R$ ' 2}} à Pagar</h2>
     <h2>Você tem {{totalReceber | currency 'R$ ' 2}} à Receber</h2>
     <br/><br/>
-    <h2>Seu saldo será {{totalReceber - totalPagar | currency 'R$ ' 2}}</h2>
+    <h2>Seu saldo será de {{totalReceber - totalPagar | currency 'R$ ' 2}}</h2>
     `,
     data: function(){
         return {
